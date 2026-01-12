31fe5b8a3984d60893b81b7095519fc2e318b36b
FaizanR-022 <faizanraza405@yahoo.com>
2026-01-12 05:31:00 +0500

    email issue resolved

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false, // true for 465, false for other ports
+  service: "gmail", // (handles host/port automatically)
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
